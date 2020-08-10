12d266ccb44a8834fea5212f4eed1666def34491
chenbo <chenbotome@163.com>
2020-08-10 14:15:27 +0800

    fix

---
+++ src/DateTime.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace tool;
+
+/**
+ * Class DateTime
+ * @package tool
+ */
+class DateTime
+{
+    /**
+     * @param $time
+     * @return string
+     */
+    public function ago($time)
+    {
+        $timeNow  = date("Y-m-d H:i:s", time());
+        $timeNow  = strtotime($timeNow);
+        $timeShow = strtotime($time);
+        $dur      = $timeNow - $timeShow;
+        if ($dur < 0) {
+            return $time;
+        } else {
+            if ($dur < 60) {
+                return $dur . '秒前';
+            } else {
+                if ($dur < 3600) {
+                    return floor($dur / 60) . '分钟前';
+                } else {
+                    if ($dur < 86400) {
+                        return floor($dur / 3600) . '小时前';
+                    } else {
+                        if ($dur < 259200) {
+                            return floor($dur / 86400) . '天前';
+                        } else {
+                            return $time;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
